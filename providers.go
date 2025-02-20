8cf99725401dc6cba8421f400d5040fcce2097ae
Morten <github@mbvlabs.com>
2025-02-20 15:06:18 +0100

    remove other providers for now

--- providers.go
+++ providers.go
@@ -3,12 +3,5 @@ package heimdall
 type Provider string
 
 const (
-	ProviderOpenAI     Provider = "openai"
-	ProviderAnthropic  Provider = "anthropic"
-	ProviderGoogle     Provider = "google"
-	ProviderPerplexity Provider = "perplexity"
-
-	anthropicBaseURL  = "https://api.anthropic.com/v1"
-	googleBaseUrl     = "https://generativelanguage.googleapis.com/v1beta:chatCompletions"
-	perplexityBaseUrl = "https://api.perplexity.ai/chat/completions"
+	ProviderOpenAI Provider = "openai"
 )
