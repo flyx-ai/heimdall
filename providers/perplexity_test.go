b7fafe18b243c7576b2773f5129cb645c5e9104c
mbv <github@mbvlabs.com>
2025-03-31 12:19:30 +0200

    add perplexity completion tests back in

--- providers/perplexity_test.go
+++ providers/perplexity_test.go
@@ -14,97 +14,98 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// func TestPerplexityModelsWithCompletion(t *testing.T) {
-// 	t.Parallel()
-//
-// 	client := http.Client{
-// 		Timeout: 2 * time.Minute,
-// 	}
-// 	perplexity := providers.NewPerplexity(
-// 		[]string{os.Getenv("PERPLEXITY_API_KEY")},
-// 	)
-//
-// 	msgs := []request.Message{
-// 		{
-// 			Role:    "system",
-// 			Content: "you are a helpful assistant.",
-// 		},
-// 		{
-// 			Role:    "user",
-// 			Content: "please make a detailed analysis of the NVIDIA's current valuation.",
-// 		},
-// 	}
-//
-// 	tests := []struct {
-// 		name string
-// 		req  request.Completion
-// 	}{
-// 		{
-// 			name: "should complete request with Sonar",
-// 			req: request.Completion{
-// 				Model:       models.Sonar{},
-// 				Messages:    msgs,
-// 				Temperature: 1,
-// 				Tags: map[string]string{
-// 					"type": "testing",
-// 				},
-// 			},
-// 		},
-// 		{
-// 			name: "should complete request with SonarPro",
-// 			req: request.Completion{
-// 				Model:       models.SonarPro{},
-// 				Messages:    msgs,
-// 				Temperature: 1,
-// 				Tags: map[string]string{
-// 					"type": "testing",
-// 				},
-// 			},
-// 		},
-// 		{
-// 			name: "should complete request with SonarReasoning",
-// 			req: request.Completion{
-// 				Model:       models.SonarReasoning{},
-// 				Messages:    msgs,
-// 				Temperature: 1,
-// 				Tags: map[string]string{
-// 					"type": "testing",
-// 				},
-// 			},
-// 		},
-// 		{
-// 			name: "should complete request with SonarReasoningPro",
-// 			req: request.Completion{
-// 				Model:       models.SonarReasoningPro{},
-// 				Messages:    msgs,
-// 				Temperature: 1,
-// 				Tags: map[string]string{
-// 					"type": "testing",
-// 				},
-// 			},
-// 		},
-// 	}
-//
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			res, err := perplexity.CompleteResponse(
-// 				context.Background(),
-// 				tt.req,
-// 				client,
-// 				nil,
-// 			)
-// 			require.NoError(
-// 				t,
-// 				err,
-// 				"CompleteResponse returned an unexpected error",
-// 				"error",
-// 				err,
-// 			)
-//
-// 			assert.NotEmpty(t, res.Content, "content should not be empty")
-// 		})
-// 	}
-// }
+func TestPerplexityModelsWithCompletion(t *testing.T) {
+	t.Parallel()
+
+	client := http.Client{
+		Timeout: 2 * time.Minute,
+	}
+	perplexity := providers.NewPerplexity(
+		[]string{os.Getenv("PERPLEXITY_API_KEY")},
+	)
+
+	msgs := []request.Message{
+		{
+			Role:    "system",
+			Content: "you are a helpful assistant.",
+		},
+		{
+			Role:    "user",
+			Content: "please make a detailed analysis of the NVIDIA's current valuation.",
+		},
+	}
+
+	tests := []struct {
+		name string
+		req  request.Completion
+	}{
+		{
+			name: "should complete request with Sonar",
+			req: request.Completion{
+				Model:       models.Sonar{},
+				Messages:    msgs,
+				Temperature: 1,
+				Tags: map[string]string{
+					"type": "testing",
+				},
+			},
+		},
+		{
+			name: "should complete request with SonarPro",
+			req: request.Completion{
+				Model:       models.SonarPro{},
+				Messages:    msgs,
+				Temperature: 1,
+				Tags: map[string]string{
+					"type": "testing",
+				},
+			},
+		},
+		{
+			name: "should complete request with SonarReasoning",
+			req: request.Completion{
+				Model:       models.SonarReasoning{},
+				Messages:    msgs,
+				Temperature: 1,
+				Tags: map[string]string{
+					"type": "testing",
+				},
+			},
+		},
+		{
+			name: "should complete request with SonarReasoningPro",
+			req: request.Completion{
+				Model:       models.SonarReasoningPro{},
+				Messages:    msgs,
+				Temperature: 1,
+				Tags: map[string]string{
+					"type": "testing",
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := perplexity.CompleteResponse(
+				context.Background(),
+				tt.req,
+				client,
+				nil,
+			)
+			require.NoError(
+				t,
+				err,
+				"CompleteResponse returned an unexpected error",
+				"error",
+				err,
+			)
+
+			assert.NotEmpty(t, res.Model, "model should not be empty")
+			assert.NotEmpty(t, res.Content, "content should not be empty")
+		})
+	}
+}
 
 func TestPerplexityModelsWithStreaming(t *testing.T) {
 	t.Parallel()
