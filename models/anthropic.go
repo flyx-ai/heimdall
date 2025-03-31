0320d392247717bcb5a982195557def052800ac0
mbv <github@mbvlabs.com>
2025-03-31 10:44:08 +0200

    cleanup

--- models/anthropic.go
+++ models/anthropic.go
@@ -4,12 +4,10 @@ const AnthropicProvider = "anthropic"
 
 type Claude3Opus struct{}
 
-// GetName implements Model.
 func (c Claude3Opus) GetName() string {
 	return "claude-3-opus-latest"
 }
 
-// GetProvider implements Model.
 func (c Claude3Opus) GetProvider() string {
 	return AnthropicProvider
 }
@@ -18,12 +16,10 @@ var _ Model = new(Claude3Opus)
 
 type Claude35Sonnet struct{}
 
-// GetName implements Model.
 func (c Claude35Sonnet) GetName() string {
 	return "claude-3-5-sonnet-latest"
 }
 
-// GetProvider implements Model.
 func (c Claude35Sonnet) GetProvider() string {
 	return AnthropicProvider
 }
@@ -32,12 +28,10 @@ var _ Model = new(Claude35Sonnet)
 
 type Claude35Haiku struct{}
 
-// GetName implements Model.
 func (c Claude35Haiku) GetName() string {
 	return "claude-3-5-haiku-latest"
 }
 
-// GetProvider implements Model.
 func (c Claude35Haiku) GetProvider() string {
 	return AnthropicProvider
 }
@@ -46,12 +40,10 @@ var _ Model = new(Claude35Haiku)
 
 type Claude37Sonnet struct{}
 
-// GetName implements Model.
 func (c Claude37Sonnet) GetName() string {
 	return "claude-3-7-sonnet-latest"
 }
 
-// GetProvider implements Model.
 func (c Claude37Sonnet) GetProvider() string {
 	return AnthropicProvider
 }
