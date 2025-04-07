e2af3a290b410f0aadd83acbba3d5f34ff7b903d
Morten Vistisen <github@mbvlabs.com>
2025-04-07 13:53:38 +0200

    feat: add support for pdf in openai models (#27)

--- heimdall.go
+++ heimdall.go
@@ -1,28 +1,47 @@
 package heimdall
 
 import (
+	"context"
 	"net/http"
 	"time"
 
-	"github.com/flyx-ai/heimdall/providers"
+	"github.com/flyx-ai/heimdall/request"
+	"github.com/flyx-ai/heimdall/response"
 )
 
+type LLMProvider interface {
+	CompleteResponse(
+		ctx context.Context,
+		req request.Completion,
+		client http.Client,
+		requestLog *response.Logging,
+	) (response.Completion, error)
+	StreamResponse(
+		ctx context.Context,
+		client http.Client,
+		req request.Completion,
+		chunkHandler func(chunk string) error,
+		requestLog *response.Logging,
+	) (response.Completion, error)
+	Name() string
+}
+
 type RouterConfig struct {
-	Providers []providers.LLMProvider
+	Providers []LLMProvider
 	Timeout   time.Duration
 }
 
 type Router struct {
-	providers map[string]providers.LLMProvider
+	providers map[string]LLMProvider
 	client    http.Client
 }
 
-func New(timeout time.Duration, llmProviders []providers.LLMProvider) *Router {
+func New(timeout time.Duration, llmProviders []LLMProvider) *Router {
 	c := http.Client{
 		Timeout: timeout,
 	}
 
-	providers := make(map[string]providers.LLMProvider, len(llmProviders))
+	providers := make(map[string]LLMProvider, len(llmProviders))
 	for _, provider := range llmProviders {
 		providers[provider.Name()] = provider
 	}
